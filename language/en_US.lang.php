495e68a0efb13d013c3f90dfef0d1d064a7f8b1a
Jeff Roedel <jeff.roedel@schoenstatt-fathers.org>
2020-11-12 13:04:17 -0600

    Add logging to stipends. Increase timeout

--- language/en_US.lang.php
+++ language/en_US.lang.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'Are you sure you want to delete the phrase "%s" from the translation database? Only delete if you\'re sure this translation is no longer used, otherwise it will reappear the next time the website encounters the same string.' => 'Are you sure you want to delete the phrase "%s" from the translation database? Only delete if you\'re sure this translation is no longer used, otherwise it will reappear the next time the website encounters the same string.',
+    'Delete translation-phrase' => 'Delete translation-phrase',
     'Edit Translation' => 'Edit Translation',
     'English (United States)' => 'English (United States)',
     'Error in form submission, please review.' => 'Error in form submission, please review.',
@@ -17,5 +19,6 @@ return [
     'Submit' => 'Submit',
     'submitted on %s by %s' => 'submitted on %s by %s',
     'Text Domain' => 'Text Domain',
+    'The form submitted did not originate from the expected site' => 'The form submitted did not originate from the expected site',
     'Translations successfully updated.' => 'Translations successfully updated.',
 ];
