9015a68e8429e6fa5020a06160e99778ded7b3b1
Jeff Roedel <jeff.roedel@schoenstatt-fathers.org>
2022-07-19 17:47:32 -0500

    Code quality

--- src/Service/CountriesFactory.php
+++ src/Service/CountriesFactory.php
@@ -1,27 +1,21 @@
 <?php
+
+declare(strict_types=1);
+
 namespace JTranslate\Service;
 
-use Laminas\ServiceManager\Factory\FactoryInterface;
-use Interop\Container\ContainerInterface;
-use Laminas\Json\Json;
 use JTranslate\Model\CountriesInfo;
+use Laminas\Json\Json;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
 
-/**
- * Factory responsible of priming the CountriesInfo service
- *
- * @author Jeff Roedel <jeff.roedel@schoenstatt-fathers.org>
- */
+use function file_get_contents;
+
 class CountriesFactory implements FactoryInterface
 {
-    /**
-     * Create an object
-     *
-     * @inheritdoc
-     */
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
         $countries = Json::decode(file_get_contents("vendor/mledoze/countries/dist/countries.json"));
-		$obj = new CountriesInfo($countries);
-		return $obj;
+        return new CountriesInfo($countries);
     }
 }
